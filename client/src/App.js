5abdc07906bab512dd7a5a9f9565d47e95f24f16
Thomas Roche <troche@linwoodhomes.com>
2023-12-03 22:59:56 -0800

    Working on getting tensorflow to work

--- client/src/App.js
+++ client/src/App.js
@@ -6,52 +6,37 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 // Import the necessary components
 import { toast } from "react-toastify";
-import {
-  Button,
-  Container,
-  Typography,
-  Grid,
-  Paper,
-  Input,
-} from "@mui/material";
+import { Button, Container, Typography, Grid, Paper } from "@mui/material";
 import { useDropzone } from "react-dropzone";
 import axios from "axios";
 
 function App() {
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [images, setImages] = useState([]); // [{ name: "image1.jpg", url: "http://localhost:5000/uploads/image1.jpg" }
-
-  const getImages = async () => {
-    const getURL = process.env.APIGET_URL || "http://localhost:5000/image-data";
-    try {
-      const response = await axios.get(getURL);
-      setImages(response.data);
-    } catch (error) {
-      console.error("Error: ", error);
-      setImages([]);
-    }
-  };
+  const [file, setFile] = useState(null);
+  const [prediction, setPrediction] = useState("");
 
   const onDrop = (acceptedFiles) => {
     setFile(acceptedFiles[0]);
   };
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const clearImage = () => {
+    setFile(null);
+    setPrediction("");
   };
 
   const handleUpload = async () => {
-    if (selectedFile) {
+    if (file) {
       const formData = new FormData();
-      formData.append("image", selectedFile);
-
-      const postURL = process.env.APIPOST_URL || "http://localhost:5000/upload";
+      formData.append("image", file);
 
       try {
-        setLoading(true);
-        await axios.post(postURL, formData);
-        setLoading(false);
+        const response = await axios.post(
+          "http://localhost:5000/upload",
+          formData
+        );
+        console.log("Response: ", response);
+        setPrediction(response.data.prediction);
         toast.success("Image uploaded successfully");
       } catch (error) {
         console.error("Error uploading image:", error);
@@ -86,44 +71,65 @@ function App() {
           >
             {/* Your centered block content */}
             <Grid style={{ marginBottom: "10px" }}>
-              <Typography variant="h2">Image Library</Typography>
+              <Typography variant="h2">Image Recognition</Typography>
               <Typography variant="h6">
                 Upload a photo and it will guess what it is
               </Typography>
             </Grid>
 
-            <Grid container>
-              <Grid item>
-                <Paper style={{ padding: "20px" }}>
-                  {/* Content for Column 1 */}
-                  <Input
-                    type="file"
-                    onChange={handleFileChange}
-                    style={{ marginRight: "10px" }}
-                  />
-                  <Button variant="contained" onClick={handleUpload}>
-                    Upload
-                  </Button>
-                </Paper>
-              </Grid>
+            <Grid>
+              <Paper style={{ padding: "20px" }}>
+                <div {...getRootProps()} style={dropzoneStyles}>
+                  <input {...getInputProps()} />
+                  {isDragActive ? (
+                    <p>Drop the files here...</p>
+                  ) : (
+                    <p>Drag and drop a file here, or click to select a file</p>
+                  )}
+                </div>
+                {file && (
+                  <div>
+                    <img
+                      src={URL.createObjectURL(file)}
+                      alt="Selected"
+                      style={{
+                        maxWidth: "300px",
+                        maxHeight: "300px",
+                        padding: "50px",
+                      }}
+                    />
+                    <br></br>
+                    <Grid container justifyContent="flex-end" spacing={2}>
+                      <Grid item>
+                        <Button variant="contained" onClick={handleUpload}>
+                          Upload and Analyze
+                        </Button>
+                      </Grid>
+                      <Grid item>
+                        <Button onClick={clearImage}>Clear</Button>
+                      </Grid>
+                    </Grid>
+                  </div>
+                )}
+                {prediction && <p>Prediction: {prediction}</p>}
+              </Paper>
             </Grid>
           </Paper>
         </Grid>
-        {loading && (
-          <Grid
-            container
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <Typography variant="h4">Uploaded Images</Typography>
-          </Grid>
-        )}
       </Grid>
 
       <ToastContainer />
     </Container>
   );
 }
+
+const dropzoneStyles = {
+  border: "2px dashed #cccccc",
+  borderRadius: "4px",
+  padding: "20px",
+  textAlign: "center",
+  cursor: "pointer",
+  marginTop: "20px",
+};
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,96 +1,57 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-const multer = require("multer");
 const tf = require("@tensorflow/tfjs-node");
-const path = require("path");
+const multer = require("multer");
+
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
 
-// Load the MobileNet model
-const modelPath =
-  "https://tfhub.dev/tensorflow/tfjs-model/mobilenet_v2/classification/4/default/1/model.json";
-let model;
-tf.loadLayersModel(modelPath)
-  .then((loadedModel) => {
-    model = loadedModel;
-    console.log("Model loaded successfully.");
-  })
-  .catch((error) => {
+async function testModelLoading() {
+  try {
+    const modelPath =
+      "https://tfhub.dev/tensorflow/tfjs-model/mobilenet_v2/classification/4/default/1/model.json";
+    const model = await tf.loadLayersModel(modelPath);
+
+    // Log the summary of the loaded model (optional)
+    model.summary();
+
+    console.log("Model loaded successfully!");
+  } catch (error) {
     console.error("Error loading model:", error);
-  });
+  }
+}
 
-// const mongodbConnection =
-//   process.env.MONGODB_URI || "mongodb://localhost:27017";
+testModelLoading();
 
-// MongoDB connection setup (replace 'your_db_url' with your actual MongoDB URL)
-// mongoose.connect(mongodbConnection, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+// API endpoint to load the model
+app.get("/load-model", (req, res) => {
+  const modelPath =
+    "https://tfhub.dev/tensorflow/tfjs-model/mobilenet_v2/classification/4/default/1/model.json";
 
-// Define a simple schema (for demonstration purposes)
-// const imageSchema = new mongoose.Schema({
-//   _id: mongoose.Schema.Types.ObjectId,
-//   name: String,
-//   imageUrl: String,
-//   prediction: String,
-// });
-
-// const Image = mongoose.model("test", imageSchema, "images");
-// const Image = mongoose.model(
-//   process.env.MONGODB_DATABASE,
-//   imageSchema,
-//   process.env.MONGODB_COLLECTION
-// );
-
-// Multer setup for handling file uploads
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-//Get all the image Data from the API endpoint
-app.get("/image-data", (req, res) => {
-  Image.find()
-    .then((data) => {
-      console.log("Data: ", data);
-      res.json(data);
+  tf.loadLayersModel(modelPath)
+    .then((loadedModel) => {
+      res.json({ message: "Model loaded successfully" });
     })
     .catch((error) => {
-      console.log("Error: ", error);
+      console.error("Error loading model:", error);
+      res.status(500).json({ error: "Failed to load model" });
     });
 });
 
-// API endpoint to upload for image to mongoDB
-// app.post("/upload", upload.single("image"), async (req, res) => {
-//   // Here you would integrate with your AI/ML model for image recognition
-//   // For simplicity, we'll just echo back the uploaded image details
-//   const { originalname, buffer } = req.file;
-//   const imageUrl = `data:image/png;base64,${buffer.toString("base64")}`;
-
-//   const newImage = new Image({
-//     _id: new mongoose.Types.ObjectId(),
-//     name: originalname,
-//     imageUrl: imageUrl,
-//     prediction: "AI/ML prediction goes here",
-//   });
-
-//   await newImage.save();
-
-//   res.json({ message: "Image uploaded successfully" });
-// });
-
-// Endpoint for image upload and recognition
-app.post("/upload", upload.single("image"), async (req, res) => {
+// API endpoint for image upload and recognition
+app.post("/upload", multer().single("image"), async (req, res) => {
   try {
-    if (!model) {
-      throw new Error("Model not loaded yet");
-    }
-
     const { buffer } = req.file;
+
+    // Load the model inside the endpoint
+    const modelPath =
+      "https://tfhub.dev/tensorflow/tfjs-model/mobilenet_v2/classification/4/default/1/model.json";
+    const model = await tf.loadLayersModel(modelPath);
+
     const imageBuffer = tf.node.decodeImage(buffer);
     const batchedImage = tf.expandDims(imageBuffer);
 
